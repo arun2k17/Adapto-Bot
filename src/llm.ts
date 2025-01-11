ac62af8fa4e979749a9e78659f6d3e0036a71720
arun2k17 <31748226+arun2k17@users.noreply.github.com>
2025-01-11 03:08:31 +0000

    use client side tool calling

--- src/llm.ts
+++ src/llm.ts
@@ -2,11 +2,25 @@ const endpoint = process.env["AZUE_AI_ENDPOINT"];
 const azureApiKey = process.env["AZURE_API_KEY"];
 const axios = require("axios");
 
-export const callLLM = async (content) => {
+// axios.interceptors.response.use(
+//     (response) => {
+//         console.log('Full response:', response);
+//         return response;
+//     },
+//     (error) => {
+//         console.error('Error response:', error);
+//         return Promise.reject(error);
+//     }
+// );
+
+export const callLLM = async (content, tools) => {
     const body = {
         messages: content,
         temperature: 0.7,
-        top_p: .95
+        top_p: .95,
+        tools: tools,
+        tool_choice: 'auto',
+        model: 'gpt-4o'
     };
     const headers = {
         "api-key": azureApiKey,
@@ -16,7 +30,7 @@ export const callLLM = async (content) => {
     const result = await axios.post(endpoint, body, {
         headers
     })
-    const json = result.data.choices[0].message.content;
+    const json = result.data.choices[0].message;
     console.log(`Result: \n\n ${JSON.stringify(json)}`)
     return json;
 }
--- src/scenarios/activity-generator.ts
+++ src/scenarios/activity-generator.ts
@@ -1,10 +1,50 @@
 import { ITeamsScenario, IScenarioBuilder } from "../teams-bot";
 import { callLLM } from "../llm";
 
+const tools = [
+    {
+        "type": "function",
+        "function": {
+            "name": "sendActivity",
+            "description": "Send botframework activity",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "text": {
+                        "type": "string",
+                        "description": "The text content of the activity"
+                    },
+                    "attachments": {
+                        "type": "array",
+                        "description": "Card attachments to the message",
+                        "items": {
+                            "type": "object",
+                            "properties": {
+                                "content": {
+                                    "type": "object",
+                                    "additionalProperties": true,
+                                    "description": "attachment content as json payload"
+                                },
+                                "contentType": {
+                                    "type": "string",
+                                    "description": "content type of the attachment"
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+];
+
 export class ActivityGenerator implements ITeamsScenario {
     public accept(teamsBot: IScenarioBuilder) {
-        teamsBot.registerTextCommand(/^generate/i, async (context) => {
-            const systemPrompt = "You are AI assistant to help with generating bot framework activity json. You need to generate Microsoft Teams bot framework activity payload for a given request. Produce the output in raw json.\n[EXAMPLE OUTPUT]\n{\n  \"type\": \"message\",\n  \"text\": \"Hello world\"\n}";
+        teamsBot.registerTextCommand(/^genAI/i, async (context) => {
+
+            // const systemPrompt = "You are AI assistant to help with generating bot framework activity json. You need to generate Microsoft Teams bot framework activity payload for a given request as a json. Produce the output in raw json.\n[EXAMPLE OUTPUT]\n{\n  \"type\": \"message\",\n  \"text\": \"Hello world\"\n}";
+            const systemPrompt = "You are AI assistant called genAI. You are a teams bot. You can generate and send bot framework activities to the user based user prompt. You can use the sendActivity tool to send an activity back to the user";
+
             const prompt = [
                 {
                     "role": "system",
@@ -15,17 +55,37 @@ export class ActivityGenerator implements ITeamsScenario {
                     "content": context.activity.text
                 }
             ]
-            const aiResult = await callLLM(prompt);
-            await context.sendActivity(`AI result\n${aiResult}`);
             try {
-                const activity = JSON.parse(aiResult);
-                await context.sendActivity(activity);
+                const aiResult = await callLLM(prompt, tools);
+                await context.sendActivity(`AI result\n${JSON.stringify(aiResult)}`);
+
+                const tool_calls = aiResult.tool_calls;
+                if (tool_calls) {
+                    for (const tool_call of tool_calls) {
+                        const fnName = tool_call.function.name;
+                        const fnArgs = JSON.parse(tool_call.function.arguments);
+                        if (fnName === "sendActivity") {
+                            await sendActivity(context, {
+                                type: "message",
+                                text: fnArgs.text,
+                                attachments: fnArgs.attachments
+                            })
+                        }
+                    }
+                } else {
+                    const activity = JSON.parse(aiResult.content);
+                    await context.sendActivity(activity);
+                }
             }
             catch (error) {
+                console.log(error);
                 await context.sendActivity(`Activity error: ${JSON.stringify(error)}`);
             }
-
         });
-
     }
+}
+
+const sendActivity = async (context, activity) => {
+    console.log(`sending activity ${JSON.stringify(activity)}`)
+    await context.sendActivity(activity);
 }
